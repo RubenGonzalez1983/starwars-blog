e0ce1f32d6c505f1c9ca9b680d7ba29cbf63780c
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-08-05 00:30:50 +0000

    added keys on cardtitle

--- src/js/views/peoplelist.js
+++ src/js/views/peoplelist.js
@@ -7,7 +7,7 @@ export function PeopleList() {
 			<div key={i} className="card" style={{ width: "18rem" }}>
 				<img src="https://via.placeholder.com/400x200.png?text=Ruben" className="card-img-top" alt="..." />
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
+					<h5 className="card-title">{person.key}</h5>
 					<p className="card-text">
 						Some quick example text to build on the card title and make up the bulk of the card&apos;s
 						content.
